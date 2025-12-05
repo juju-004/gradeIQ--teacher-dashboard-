080d83bc34ca8aee03a323a0195f1ea427da05c0
David Udoh <udohdavid004@gmail.com>
2025-12-05 13:10:20 +0100

    fix: five

--- src/app/api/admin/add-bulk/route.ts
+++ src/app/api/admin/add-bulk/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { NextResponse } from "next/server";
 import { connectDB, User } from "@/server/db";
 import { getSession } from "@/server/actions";
